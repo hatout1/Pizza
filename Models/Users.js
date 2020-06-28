234c227c5cd5c844022c97a13499f7349200bf1a
hatout01 <hatout1@gmail.com>
2020-06-28 10:31:55 -0700

    added bcrypt and update User file

--- Models/Users.js
+++ Models/Users.js
@@ -51,4 +51,25 @@ let UserSchema = new Schema({
   ],
 });
 
-module.exports = ("User", UserSchema);
+UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  bcrypt.hash(this.password, 10, (err, passwordHash) => {
+    if (err) return next(err);
+    this.password = passwordHash;
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    else {
+      if (!isMatch) return cb(null, isMatch);
+      return cb(null, this);
+    }
+  });
+};
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
